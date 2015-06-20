e3740954d9ed2a46f0ac75ddb292644f1f5ea938
miytasuki <miytasuki@gmail.com>
2015-06-21 01:10:33 +0900

    いつからカウントするかユーザが指定できるようにした

--- yammerCount.js
+++ yammerCount.js
@@ -1,5 +1,10 @@
 javascript: (function() {
+   
+    var dateString = window.prompt("‚¢‚ÂˆÈ~‚Ì”‚ğ”‚¦‚½‚¢‚©yyyy/MM/dd hh:mmŒ`®‚Å“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n—á2015/06/20 19:00");
+    
+    var countFrom = new Date(dateString);
     var d = new Date();
+    
     var threadIds = [];
     var threadTitle = [];
     var threadCounter = [];
@@ -7,6 +12,12 @@ javascript: (function() {
     var start = Date.now();
     var myId;
     var alertText = "";
+    var completeSeen = false;
+    var completeUnseen = false;
+    var completeBoth = false;
+        
+    $("body").empty();
+    $("body").append("<table></table>");
 
     function getMyMessage(n, last, currentThread) {
         $.ajax({
@@ -17,18 +28,18 @@ javascript: (function() {
                 var i;
                 for (i = 0; i < data.messages.length; i++) {
                     d.setTime(Date.parse(data.messages[i].created_at));
-                    var date = d.getDate();
-                    d.setTime(Date.now());
-                    if (data.messages[i].sender_id === myId && date === d.getDate()) {
+//                    var date = d.getDate();
+//                    d.setTime(Date.now());
+                    if (data.messages[i].sender_id === myId && countFrom < d) {
                         count++;
                         threadCounter[currentThread]++;
                     }
-                }
-                if (date !== d.getDate()) {
+                };
+                if (countFrom > d) {
                     n++;
                 }
-                if (n < 1) {
-                    getMyMessage(0, data.messages[data.messages.length - 1].id, currentThread);
+                if (n < 1 && data.messages.length === 20) {
+                    getMyMessage(0, data.messages[19].id, currentThread);
                 } else if (currentThread < threadIds.length - 1) {
                     currentThread++;
                     getMyMessage(0, false, currentThread);
@@ -50,24 +61,45 @@ javascript: (function() {
     function get(url, n, last) {
         console.log("fetching thread ids...");
         $.ajax({
-            url: url,
+            url: url + (last ? "&older_than=" + last : ""),
             dataType: "json",
             success: function(data) {
                 var i;
+                var isFinalSearch = false;
                 for (i = 0; i < data.messages.length; i++) {
-                    d.setTime(Date.parse(data.messages[i].created_at));
-                    var date = d.getDate();
-                    d.setTime(Date.now());
-                    if (threadIds.indexOf(data.messages[i].thread_id) === -1) {
-                        threadIds[threadIds.length] = data.messages[i].thread_id;
+                    if (data.threaded_extended[data.messages[i].thread_id].length > 0) {
+                        d.setTime(Date.parse(data.threaded_extended[data.messages[i].thread_id][0].created_at));
+                    } else {
+                        d.setTime(Date.parse(data.messages[i].created_at));
                     }
-                    threadTitle.push(data.messages[i].content_excerpt.split("\n")[0]);
-                    threadCounter.push(0);
+//                    var date = d.getDate();
+//                   d.setTime(Date.now());
+                    if (countFrom < d) {
+                        threadIds[threadIds.length] = data.messages[i].thread_id;
+                        threadTitle.push(data.messages[i].content_excerpt.split("\n")[0]);
+                        threadCounter.push(0);
+                        $("table").append("<tr><td>%1</td></tr>".replace("%1", data.messages[i].content_excerpt.split("\n")[0]));
+                        $("table").append("<tr><td>%1</td></tr>".replace("%1", d.getDate()));
+                    } else {
+                        isFinalSearch = true;
+                    }
                 }
-                if (n === 1) {
-                    console.log(data);
+                if (n == 1) {
+                    completeSeen = true;
+                } else {
+                    completeUnseen = true;
+                }
+                if (!isFinalSearch && data.messages.length === 20) {
+                    if (data.threaded_extended[data.messages[19].thread_id].length > 0) {
+                        get(url, n, data.threaded_extended[data.messages[19].thread_id][0].id);
+                    } else {
+                        get(url, n, data.messages[19].thread_id);
+                    }
+                } else if (completeSeen && completeUnseen && !completeBoth) {
                     getMyMessage(0, false, 0);
+                    completeBoth = true;
                 }
+
             }
         });
     }
@@ -75,8 +107,8 @@ javascript: (function() {
         url: "https://www.yammer.com/api/v1/users/current.json",
         success: function(data) {
             myId = data.id;
-            get("https://www.yammer.com/api/v1/messages/inbox.json?filter=inbox_unseen", 0);
-            get("https://www.yammer.com/api/v1/messages/inbox.json?filter=inbox_seen", 1);
+            get("https://www.yammer.com/api/v1/messages/inbox.json?filter=inbox_unseen", 1);
+            get("https://www.yammer.com/api/v1/messages/inbox.json?filter=inbox_seen", 0);
         }
     });
 })();
